5667958774c22165caae761529bb64abf229be4c
PhyTaffa <filippotaffarello@gmail.com>
2025-01-05 05:26:12 +0000

    ewq245thrsd

--- database.js
+++ database.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); // Load environment variables
+
 const mysql = require('mysql2');
 
-// Create a new connection to the database
+// Create a new connection pool
 const pool = mysql.createPool({
 
     //localHost:
@@ -9,18 +11,23 @@ const pool = mysql.createPool({
     // password: '123',
     // database: 'serve-the-truth' 
 
-
     //Filess.io:
+    // host: '9nrms.h.filess.io',
+    // user: 'ServeTheTruth_individual', 
+    // port: "3307",
+    // password: 'b30aa021bd77a83b6cb81330dc52e0719f677bce',
+    // database: 'ServeTheTruth_individual',
 
-    host: '9nrms.h.filess.io',
-    user: 'ServeTheTruth_individual', 
-    port: "3307",
-    password: 'b30aa021bd77a83b6cb81330dc52e0719f677bce',
-    database: 'ServeTheTruth_individual',
-    waitForConnections: true, // Will wait if no connection is available
-    connectionLimit: 2,      // Match the connection limit of your online DB
-    queueLimit: 0            // No limit on queue size (can be adjusted)
-
+    
+    //better practise, but i will leave the rest just to be sure
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 2,    // Match the connection limit of your online DB
+    queueLimit: 0          // No limit on queue size (can be adjusted)
 });
 
 // Use promise-based API for convenience
