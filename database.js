82168c7afb47d17922a864bf624fe6ed86cb8058
PhyTaffa <filippotaffarello@gmail.com>
2025-01-05 02:52:46 +0000

    now with online db

--- database.js
+++ database.js
@@ -4,19 +4,19 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
 
     //localHost:
-    host: 'localhost',
-    user: 'root',
-    password: '123',
-    database: 'serve-the-truth' 
+    // host: 'localhost',
+    // user: 'root',
+    // password: '123',
+    // database: 'serve-the-truth' 
 
 
     //Filess.io:
 
-    // host: '9nrms.h.filess.io',
-    // user: 'ServeTheTruth_individual', 
-    // port: "3307",
-    // password: 'b30aa021bd77a83b6cb81330dc52e0719f677bce',
-    // database: 'ServeTheTruth_individual' 
+    host: '9nrms.h.filess.io',
+    user: 'ServeTheTruth_individual', 
+    port: "3307",
+    password: 'b30aa021bd77a83b6cb81330dc52e0719f677bce',
+    database: 'ServeTheTruth_individual' 
 
 });
 
