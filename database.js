62a2381b6fcf1d923c92b801cbd0ca4a721f94f5
PhyTaffa <filippotaffarello@gmail.com>
2025-01-05 15:39:23 +0000

    asdfghjkl

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load environment variables
+//require('dotenv').config(); // Load environment variables
 
 const mysql = require('mysql2');
 
@@ -13,11 +13,11 @@ const pool = mysql.createPool({
 
     
     //Filess.io:
-    // host: '9nrms.h.filess.io',
-    // user: 'ServeTheTruth_individual', 
-    // port: "3307",
-    // password: 'b30aa021bd77a83b6cb81330dc52e0719f677bce',
-    // database: 'ServeTheTruth_individual',
+    host: '9nrms.h.filess.io',
+    user: 'ServeTheTruth_individual', 
+    port: "3307",
+    password: 'b30aa021bd77a83b6cb81330dc52e0719f677bce',
+    database: 'ServeTheTruth_individual',
 
 
     //aiven:
@@ -37,11 +37,11 @@ const pool = mysql.createPool({
     // password: '',
     
     //better practise, but i will leave the rest just to be sure
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+    // host: process.env.DB_HOST ,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
+    // database: process.env.DB_NAME,
 
     waitForConnections: true,
     connectionLimit: 2,    // Match the connection limit of your online DB
